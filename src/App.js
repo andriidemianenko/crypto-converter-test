aaa40d26fc128b56d70e5722f7a37ea7e7559e3f
Andrei Demianenko <andrei.demianenko@tagsoft.co>
2020-03-06 16:51:50 +0200

    change currency + calculate volume + change crypto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { useDispatch, useSelector, shallowEqual } from 'react-redux'
+import React, { useContext, useState, useEffect, useReducer } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { getCurrencyRates } from './store/actions'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -19,12 +19,17 @@ function useCryptoWithRates(cryptos, currencyRates, loading) {
 }
 
 function App() {
-  const [volume, setVolume] = useState(0)
   const [validationErrorExists, setValidationError] = useState(false)
-  const [currentCurrency, chooseCurrency] = useState('UAH')
+  const [volume, setVolume] = useState({ default: 0, volumeInCurrency: 0 })
+  const [choosenCurrency, chooseCurrency] = useState('UAH')
+  const [choosenCrypto, chooseCrypto] = useState('BTC')
 
-  const { colors: { mainBackground, lightGreen, borderBlue }, text: { defaultFontSize } } = useContext(context.themeContext)
+  const { 
+    colors: { mainBackground, lightGreen, borderBlue, lightBlue1 },
+    text: { defaultFontSize, currencyFontWeight }
+  } = useContext(context.themeContext)
   const cryptos = useContext(context.cryptoCurrenciesContext)
+  const currencies = useContext(context.currencies)
 
   const loading = useSelector(state => state.areCurrencyRatesLoading)
   const currencyRates = useSelector(state => state.currencyRates)
@@ -36,10 +41,28 @@ function App() {
     dispatch(getCurrencyRates())
   }, [dispatch])
 
+  useEffect(() => {
+    if (volume.default) validateAndSetVolume(volume.default)
+  }, [choosenCrypto, choosenCurrency])
+
   function validateAndSetVolume(volume) {
     const isNumber = !isNaN(parseFloat(volume)) && isFinite(volume) && !(/\s/.test(volume))
     if (isNumber) {
       setValidationError(false)
+      const { uahValue, rubValue, usdValue } = cryptosWithRate.find(crypto => crypto.name === choosenCrypto)
+      switch (choosenCurrency) {
+        case 'UAH':
+          setVolume({ default: volume, volumeInCurrency: Number(volume * uahValue).toFixed(3) })
+          break
+        case 'USD':
+          setVolume({ default: volume, volumeInCurrency: Number(volume * usdValue).toFixed(3) })
+          break
+        case 'RUB':
+          setVolume({ default: volume, volumeInCurrency: Number(volume * rubValue).toFixed(3) })
+          break
+        default:
+          return
+      }
     } else {
       setValidationError(true)
       return
@@ -49,39 +72,65 @@ function App() {
   return (
     <Container
       style={{ background: mainBackground, fontSize: defaultFontSize }}
-      className="py-5 vh-100"
+      className="py-5 vh-100 text-center"
       fluid
      >
-      <Spinner animation="grow" size="lg" />
-
+      {loading && <Spinner animation="grow" size="lg" style={{ width: '400px', height: '400px', color: lightGreen }}/>}
       {!loading && 
-        <Row className="justify-content-center mb-4">
-          {cryptosWithRate.map(crypto => {
-            return (
-              <Col xs={3} key={`${crypto.name}-id`}>
-                <CryptoButton cryptoData={crypto} />
+        <div>
+          <Row className="justify-content-center mb-4">
+            {cryptosWithRate.map(crypto => {
+              return (
+                <Col xs={3} key={`${crypto.name}-id`} onClick={() => chooseCrypto(crypto.name) }>
+                  <CryptoButton cryptoData={{ ...crypto, isCryptoChoosen: choosenCrypto === crypto.name }} />
+                </Col>
+              )
+            })}
+          </Row>
+          <Form className="mt-5 mb-3">
+            <Form.Group  className="justify-content-center" as={Row}>
+              <Form.Label column xs={1} style={{ color: lightGreen }}>Volume:</Form.Label>
+              <Col xs={6}>
+                <Form.Control
+                  style={{ backgroundColor: mainBackground, borderColor: borderBlue, color: lightGreen }}
+                  onChange={({ target }) => validateAndSetVolume(target.value)}
+                  type="text"
+                />
+                {validationErrorExists && 
+                  <Form.Text className="text-danger">
+                    Volume must be a number!
+                  </Form.Text>
+                }
               </Col>
-            )
-          })}
-        </Row>
-      }
-      <Form className="my-5">
-        <Form.Group  className="justify-content-center" as={Row}>
-          <Form.Label column xs={1} style={{ color: lightGreen }}>Volume:</Form.Label>
-          <Col xs={6}>
-            <Form.Control
-              style={{ backgroundColor: mainBackground, borderColor: borderBlue, color: lightGreen }}
-              onChange={({ target }) => validateAndSetVolume(target.value)}
-              type="text"
-            />
-            {validationErrorExists && 
-              <Form.Text className="text-danger">
-                Volume must be a number!
-              </Form.Text>
-            }
-          </Col>
-        </Form.Group>
-      </Form>
+            </Form.Group>
+          </Form>
+          {volume.default &&
+            <Row className="justify-content-center">
+              <p style={{ color: lightGreen }}>
+                  <b className="pr-2">{volume.default}{choosenCrypto}</b>
+                  will be
+                  <b className="px-2">{volume.volumeInCurrency}</b>
+                  in
+                  <b className="pl-2">{choosenCurrency}</b>
+              </p>
+            </Row>
+          }
+          <Row className="justify-content-center">
+            {currencies.map(currency =>
+                <Col xs={1} className="px-2" onClick={() => chooseCurrency(currency)} key={`${currency}-currency-name`}>
+                <div className="px-2 py-3"
+                  style={{
+                    backgroundColor: choosenCurrency === currency ? lightGreen : lightBlue1,
+                    color: choosenCurrency === currency ? lightBlue1 : lightGreen,
+                    fontWeight: currencyFontWeight
+                  }}>
+                  <span>{currency}</span>
+                </div>
+              </Col>
+            )}
+          </Row>
+      </div>
+    }
     </Container>
   )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,8 +2,10 @@ import { createContext } from 'react'
 
 import theme from './theme'
 import cryptoCurrencies from './cryptoCurrencies'
+import currencies from './currencies'
 
 export const context = {
   themeContext: createContext(theme),
-  cryptoCurrenciesContext: createContext(cryptoCurrencies)
+  cryptoCurrenciesContext: createContext(cryptoCurrencies),
+  currencies: createContext(currencies)
 }
